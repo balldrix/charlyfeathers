862e23e496c9ca9a4b785f147d07b524eea4b6a9
balldrix <cballdev@outlook.com>
2024-07-24 21:59:09 +0100

    add empty pagination component

---
+++ charlyfeathers.client/src/app/catalog/pagination/pagination.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cfs-pagination',
+  standalone: true,
+  imports: [],
+  templateUrl: './pagination.component.html',
+  styleUrl: './pagination.component.css'
+})
+export class PaginationComponent {
+
+}
